44efd4605d762d18b87ed0db5220c0943c66ba42
HP <HP@LAPTOP-AK9U0JDU>
2022-01-19 10:04:18 +0500

    youtube Downloader is working

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using VideoLibrary;
 
 namespace YouTubeDownloader
 {
@@ -6,7 +10,27 @@ namespace YouTubeDownloader
     {
         static void Main(string[] args)
         {
-            
+            string video_url = "https://www.youtube.com/watch?v=RWYrafpP53A";
+
+            Console.WriteLine("Videoni Kachestvasini kiriting : ");
+            int video_size =int.Parse(Console.ReadLine());
+
+            if (video_size != 720 || video_size != 360 || video_size != 1080)
+                video_size = 720;
+
+            YouTube yt = YouTube.Default;
+
+            List<YouTubeVideo> videos = yt.GetAllVideos(video_url).ToList();
+
+            YouTubeVideo video = videos.FirstOrDefault(p => p.Resolution == 360 && p.FileExtension == ".mp4");
+
+            Console.WriteLine("Boshlandi...");
+
+            byte[] file = video.GetBytes();
+
+            File.WriteAllBytes(@"D:\xira.mp4", file);
+
+            Console.WriteLine("Downloaded ");
         }
     }
 }
